c1d827084e63042c20574f3ac727e21abcc8867e
Gealor <gzazulya@gmail.com>
2026-02-10 19:35:49 +0400

    refactor/added-config-file

---
+++ lab_1/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import numpy as np
+
+# Files and dirs
+PAINTINGS_DIR_NAME = "paintings"
+BASE_DIR = Path(__file__).parent
+
+MET_OBJECTS_FILE = BASE_DIR / "MetObjects.csv"
+PAINTING_CLASSIFICATION = "Paintings"
+
+ORIGINAL_IMAGE = "original.jpg"
+
+
+# Integration config
+BASE_URL = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+
+
+# Image processing
+KERNEL_GAUSSIAN = np.array([
+    [1,  4,  7,  4, 1],
+    [4, 16, 26, 16, 4],
+    [7, 26, 41, 26, 7],
+    [4, 16, 26, 16, 4],
+    [1,  4,  7,  4, 1]
+], dtype=np.float32)
