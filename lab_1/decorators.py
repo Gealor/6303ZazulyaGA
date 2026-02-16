fd54c5f8fa427eaf80e2bc81067c51dbf1c45afd
Gealor <gzazulya@gmail.com>
2026-02-16 19:18:27 +0400

    fix/change-int-to-float-number-in-time-meter-decorator

--- lab_1/decorators.py
+++ lab_1/decorators.py
@@ -9,7 +9,7 @@ def time_meter_decorator(func: Callable):
         start = time.perf_counter()
         result = func(*args, **kwargs)
         execution_time = time.perf_counter() - start
-        log.info("Функция %s выполнилась за %d секунд", func.__name__, execution_time)
+        log.info("Функция %s выполнилась за %f секунд", func.__name__, execution_time)
         return result
 
     return wrapper
